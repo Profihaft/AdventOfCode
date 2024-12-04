b65f18ac30ad11d8f6dc7791afea4a97dc2fc420
Emil Hödl <emil.hoedl@it.htl-hl.ac.at>
2024-12-04 13:00:22 +0100

    Day 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod day02{
+    pub mod day02;
+}
 mod day03{
     pub mod day03;
 }
@@ -5,10 +8,14 @@ mod day04{
     pub mod day04;
 }
 
-use day04::day04::*;
+use day02::day02::*;
 use day03::day03::*;
+use day04::day04::*;
 
 fn main() {
+    println!("--- Day 2 ---");
+    day_two_part_one().unwrap();
+    day_two_part_two().unwrap();
     println!("--- Day 3 ---");
     day_three(r"src\day03\day03_input.txt");
     println!("--- Day 4 ---");
