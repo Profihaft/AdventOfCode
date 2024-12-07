9e37fc8609b0da2064fe799ce9e9ff4d9596d9bd
Emil Hödl <emil.hoedl@it.htl-hl.ac.at>
2024-12-07 08:46:11 +0100

    Day 7

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,31 @@
-mod day01 {
-    pub mod day01;
-}
-mod day02 {
-    pub mod day02;
-}
-mod day03{
-    pub mod day03;
-}
-mod day04{
-    pub mod day04;
-}
-mod day05 {
-    pub mod day05;
-}
-mod day06{
-    pub mod day06;
-}
-
-use day01::day01::*;
-use day02::day02::*;
-use day03::day03::*;
-use day04::day04::*;
-use day05::day05::*;
-use day06::day06::*;
+#[path = "day01/day01.rs"]
+mod day01;
+#[path = "day02/day02.rs"]
+mod day02;
+#[path = "day03/day03.rs"]
+mod day03;
+#[path = "day04/day04.rs"]
+mod day04;
+#[path = "day05/day05.rs"]
+mod day05;
+#[path = "day06/day06.rs"]
+mod day06;
+#[path = "day07/day07.rs"]
+mod day07;
 
 fn main() {
     println!("--- Day1 ---");
-    day_one(r"src\day01\day01_input.txt").unwrap();
+    day01::day_one(r"src\day01\day01_input.txt").unwrap();
     println!("--- Day 2 ---");
-    day_two(r"src\day02\day02_input.txt").unwrap();
+    day02::day_two(r"src\day02\day02_input.txt").unwrap();
     println!("--- Day 3 ---");
-    day_three(r"src\day03\day03_input.txt");
+    day03::day_three(r"src\day03\day03_input.txt");
     println!("--- Day 4 ---");
-    day_four(r"src\day04\day04_input.txt");
+    day04::day_four(r"src\day04\day04_input.txt");
     println!("--- Day 5 ---");
-    day_five(r"src\day05\day05_input.txt");
+    day05::day_five(r"src\day05\day05_input.txt");
     println!("--- Day 6 ---");
-    day_six(r"src\day06\day06_input.txt").unwrap();
+    day06::day_six(r"src\day06\day06_input.txt").unwrap();
+    println!("--- Day 7 ---");
+    day07::day_seven(r"src\day07\day07_input.txt");
 }
